8c348941f69e6f179253c6c2fd89d5fe7b99ce4a
Joe Bosman <joseph.a.bosman@gmail.com>
2017-05-05 15:13:54 -0400

    Added responsive website course to study section.

---
+++ browser/js/study/directives/sections/buildResponsiveSitesCourse/buildResponsiveSites.js
@@ -0,0 +1,6 @@
+app.directive('buildResponsiveSitesCourse', () => {
+	return {
+		restrict: 'E',
+		templateUrl: 'js/study/directives/sections/buildResponsiveSitesCourse/buildResponsiveSites.html'
+	}
+});
