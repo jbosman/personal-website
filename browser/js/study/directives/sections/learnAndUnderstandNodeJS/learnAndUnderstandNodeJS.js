17f77e18ec38e3e4f9915e03b53b83bd8b8a9159
Joe Bosman <joseph.a.bosman@gmail.com>
2017-05-05 17:39:27 -0400

    Added Modern React With Redux course to study section

---
+++ browser/js/study/directives/sections/learnAndUnderstandNodeJS/learnAndUnderstandNodeJS.js
@@ -0,0 +1,6 @@
+app.directive('learnAndUnderstandNodeJs', () => {
+	return {
+		restrict: 'E',
+		templateUrl: 'js/study/directives/sections/learnAndUnderstandNodeJS/learnAndUnderstandNodeJS.html'
+	}
+});
---
+++ browser/js/study/directives/sections/modernReactWithRedux/modernReactWithRedux.js
@@ -0,0 +1,6 @@
+app.directive('modernReactWithRedux', () => {
+	return {
+		restrict: 'E',
+		templateUrl: 'js/study/directives/sections/modernReactWithRedux/modernReactWithRedux.html'
+	}
+})
