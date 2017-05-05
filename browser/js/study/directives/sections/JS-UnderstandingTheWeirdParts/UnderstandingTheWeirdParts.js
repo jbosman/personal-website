e264b125667cd27327e8b550ccf95fafcd783ff3
Joe Bosman <joseph.a.bosman@gmail.com>
2017-05-05 17:00:11 -0400

    Added JS Understanding the Werid Parts to study section

---
+++ browser/js/study/directives/sections/JS-UnderstandingTheWeirdParts/UnderstandingTheWeirdParts.js
@@ -0,0 +1,8 @@
+app.directive('understandingTheWeirdParts', () => {
+
+	return {
+		restrict: 'E',
+		templateUrl: 'js/study/directives/sections/JS-UnderstandingTheWeirdParts/UnderstandingTheWeirdParts.html'
+	}
+
+});
